e3a1c6253e81071f1900af902affd486903a32e7
clarapastore <clarapastore17@gmail.com>
2022-12-01 17:41:00 +0100

    Update day_1.clj

--- day_1.clj
+++ day_1.clj
@@ -2,7 +2,7 @@
  (:require [clojure.string :as str]))
 
 ;; input is a string called "input-1" (removed for brevity)
-;; string looks like this: ""
+;; string looks like this: "5557\n6663\n3952\n4750\n2271\n6653\n4406\n\n4256\n6871\n6720..."
 
 ;; goal 1: find the elf which carries the most amount of calories
 
